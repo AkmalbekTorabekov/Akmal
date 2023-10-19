3609ba7f1dab857f4f4b2bd1cfc85f78a7d55a1a
AkmalbekTorabekov <atorabekov705>
2023-10-19 16:48:24 +0500

    app.py

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,8 +4,8 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("help", "Yordam"),
+            types.BotCommand("start", "Botni ishga tushurish 😊"),
+            # types.BotCommand("help", "Yordam"),
         ]
     )
     
